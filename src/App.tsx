7c2b514c4c209774634db95ced6a6ec138c5943d
kamusari <lucker.de.souza@gmail.com>
2020-08-12 01:33:56 -0300

    chore: react init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import GlobalStyles from "./Styles/GlobalStyles";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <h1>ola mundo</h1>
+    </>
+  );
+}
+
+export default App;
